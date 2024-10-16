24715a80d63a9663b930ae8a4bba4f24f65052b1
Amrkhwazir <amrkh5678@gmail.com>
2024-10-16 13:33:31 +0500

    java learning variables

---
+++ variables/variable.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class variable {
+
+public static void main(String[] args) {
+
+// output
+// System.out.print("Hello World 334\n");
+// System.out.print("Hello World 324");
+// System.out.println("Hello World 324");
+
+
+// variables
+
+// primitive
+// byte  8bits
+// short 
+// char 2bits
+// boolean 1 bits
+// int 4bits
+// float 4bits
+// long 8bits
+// double 8bits
+
+//non primitive
+// String
+// Array
+// Object
+// Interface
+// Class
+
+// int a = 10;
+// int b = 25;
+// int sum = a + b;
+// int diff = a - b;
+// int multiply = a * b;
+// int divide = a / b;
+
+// System.out.println(multiply);
+// System.out.println(divide);
+// System.out.println(diff);
+// System.out.println(sum);
+
+// Inputs
+// Scanner sc = new Scanner(System.in);
+// String username = sc.nextLine();
+// System.out.println(username);
+
+// Scanner inputs = new Scanner(System.in);
+// int a = inputs.nextInt();
+// int b = inputs.nextInt();
+// int sum = a + b;
+// System.out.println(sum);
+
+}
+    
+}
